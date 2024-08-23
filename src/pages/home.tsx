95de662f19608ceeef11f5ae6ec310fb5ddeb171
Pujan Modha <pujanmodha19@gmail.com>
2024-08-23 17:38:40 +0530

    disabled unavailable components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,10 @@
 import Hero from "../sections/hero";
 import Overview from "@/sections/overview.jsx";
 import Sponsors from "@/sections/sponsors.jsx";
-import Timeline from "@/sections/timeline.jsx";
 import Objectives from "@/sections/objectives.jsx";
-import Mentors from "@/sections/mentors.jsx";
-import Judges from "@/sections/judges.jsx";
 import Faq from "@/sections/faq.jsx";
 import Contact from "@/sections/contact.jsx";
+
 export default function Home() {
   return (
     <>
@@ -17,10 +15,10 @@ export default function Home() {
         <div className="grid grid-cols-1 my-16 lg:my-4">
           <Overview />
           <Sponsors />
-          <Timeline />
+          {/*<Timeline />*/}
           <Objectives />
-          <Mentors />
-          <Judges />
+          {/*<Mentors />*/}
+          {/*<Judges />*/}
           <Faq />
           <Contact />
         </div>
