34574b4f74d208278acd3ba1aa031269e9ac0b4a
Pujan Modha <pujanmodha19@gmail.com>
2024-09-20 02:50:13 +0530

    add: khud ki photu

--- src/components/team_member.tsx
+++ src/components/team_member.tsx
@@ -1,40 +1,132 @@
-import { faSquareInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faSquareGithub,
+  faSquareInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
-export default function TeamMember({ name, position, image, links = {
+export default function TeamMember({
+  name,
+  position,
+  image,
+  links = {
     ig: undefined,
     gh: undefined,
-    li: undefined
-} }: {
-    name: string, position: string, image: string, links?: {
-        ig: string | undefined,
-        gh: string | undefined,
-        li: string | undefined
-    }
+    li: undefined,
+  },
+}: {
+  name: string;
+  position: string;
+  image: string;
+  links?: {
+    ig?: string;
+    gh?: string;
+    li?: string;
+  };
+  isRound?: boolean;
 }) {
-    return (
-        <div className="mx-auto">
-            <div className="mx-auto w-64 bg-ieeeorange bg-opacity-10 hover:-translate-y-6 duration-500 hover:shadow-lg hover:shadow-[#75787B] border-brand rounded-xl">
-                <div className="relative overflow-hidden transition duration-500 transform rounded-lg">
-                    <img src={image} alt={name} height={512} width={512} className="object-cover rounded-lg w-full aspect-[3/4]" loading="lazy" />
-                    <div className="absolute inset-0 flex flex-col justify-end px-4 py-2 transition-opacity duration-300 bg-[#292929] bg-opacity-40 opacity-0  hover:opacity-100">
-                        <div className="justify-center">
-                            <ul className="flex flex-col items-start text-white text-base font-medium">
-                                {links.ig && <li className="mb-2"><a href={links
-                                    .ig} target="_blank" rel="noopener noreferrer" className="hover:text-brand flex gap-1 items-center"><FontAwesomeIcon icon={faSquareInstagram} />Instagram</a></li>}
-                                {links.gh && <li className="mb-2"><a href={links
-                                    .gh} target="_blank" rel="noopener noreferrer" className="hover:text-brand flex gap-1 items-center"><FontAwesomeIcon icon={faGithub} /> Github</a></li>}
-                                {links.li && <li className="mb-2"><a href={links
-                                    .li} target="_blank" rel="noopener noreferrer" className="hover:text-brand flex gap-1 items-center"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</a></li>}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div
+        className={`mx-auto w-64 border-2 p-2 bg-brand rounded-lg bg-opacity-10 border-brand `}
+      >
+        <div
+          className={`relative overflow-hidden transition duration-500 transform`}
+        >
+          <img
+            src={image}
+            alt={name}
+            width={256}
+            height={256}
+            className={`object-cover w-full rounded-xl aspect-[3/4]`}
+          />
+          <div
+            id="grids"
+            className="absolute bg-brand rounded-xl inset-0 flex flex-col justify-end px-4 py-2 transition-opacity duration-300 bg-opacity-100 opacity-0 hover:opacity-100"
+          >
+            <div className="justify-center">
+              <ul className="flex flex-col items-start text-white text-base font-medium">
+                {links.gh && (
+                  <li>
+                    <a href={links.gh} className="inline-flex gap-2">
+                      <FontAwesomeIcon icon={faSquareGithub} size="xl" />
+                      <p className="inline-flex hover:underline hover:underline-offset-2">
+                        GitHub
+                        <span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                      </p>
+                    </a>
+                  </li>
+                )}
+                {links.li && (
+                  <li>
+                    <a href={links.li} className="inline-flex gap-2">
+                      <FontAwesomeIcon icon={faLinkedin} size="xl" />
+                      <p className="inline-flex hover:underline hover:underline-offset-2">
+                        Linkedin
+                        <span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                      </p>
+                    </a>
+                  </li>
+                )}
+                {links.ig && (
+                  <li>
+                    <a href={links.ig} className="inline-flex gap-2">
+                      <FontAwesomeIcon icon={faSquareInstagram} size="xl" />
+                      <p className="inline-flex hover:underline hover:underline-offset-2">
+                        Instagram
+                        <span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                      </p>
+                    </a>
+                  </li>
+                )}
+              </ul>
             </div>
-            <div className="my-4">
-                <h3 className="text-white text-xl font-bold">{name}</h3>
-                <p className="text-white text-sm">{position}</p>
-            </div>
+          </div>
         </div>
-    )
+      </div>
+      <div className="my-4">
+        <p className="text-center text-xl font-semibold">{name}</p>
+        <p className="text-center font-medium text-ieeegray">{position}</p>
+      </div>
+    </div>
+  );
 }
--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -4,7 +4,7 @@ import tdata from '../data/teacher_data.json'
 
 import { useState } from 'react'
 
-export default function () {
+export default function Team () {
     const [isStudent, setIsStudent] = useState(true)
     return (
         <div className="mt-20 max-w-[80rem] mx-auto py-12 px-4">
@@ -22,7 +22,7 @@ export default function () {
                                 Convenor
                             </h2>
                             <div className="mt-1 border border-brand opacity-10 mb-10"></div>
-                            <div className="grid md:grid-cols-3 grid-cols-1 gap-16 mx-auto justify-center">
+                            <div className="grid md:grid-cols-4 grid-cols-1 gap-16 mx-auto justify-center">
                                 {data.filter(item => item.Committee.includes("Convenor")).map((item, index) => <TeamMember key={index} name={item.Name} position={item['Team name and position']} image={item.image} links={{
                                     ig: item['Instagram '],
                                     gh: item['github'],
